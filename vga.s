eb0a61da857423a3a1d8aedc3030a1fe229ef3a6
Rushab Kumar <rushab3737@gmail.com>
2014-03-18 11:49:09 -0400

    Update vga.s
    Added support for drawing anywhere on screen. Fixed sp.

--- vga.s
+++ vga.s
@@ -5,6 +5,8 @@
 #	y->240
 #---------------------------------------------------#
 
+.include "nios_macros.s"
+
 .equ ADDR_VGA, 0x08000000
 .equ ADDR_CHAR, 0x09000000
 .equ COL_WHITE, 0xffff
@@ -18,27 +20,30 @@ _start:
 	#movi r11, 
 	#movi r12,
 	#sthio r4,1032(r8) /* pixel (4,1) is x*2 + y*1024 so (8 + 1024 = 1032) */
+	movia sp, 0x007ffffc
 	call erase_screen
-	subi sp, sp, 12
-	movia r4, 40
-	movia r5, 40
+	subi sp, sp, 16
+	movi r4, 40
+	movi r5, 40
 	movui r15, 0xffff  /* White pixel */
 	stw r4, 0(sp)
 	stw r5, 4(sp)
 	stw r15, 8(sp)
+	stw ra, 12(sp)
 	call draw_rect
+	ldw ra, 12(sp)
 	addi sp, sp, 12
 	movia r9, ADDR_CHAR
 	movi  r5, 0x41   /* ASCII for 'A' */
 	sthio r15,1032(r8) /* pixel (4,1) is x*2 + y*1024 so (8 + 1024 = 1032) */
-	stbio r5,132(r9) /* character (4,1) is x + y*128 so (4 + 128 = 132) */
+	#stbio r5,132(r9) /* character (4,1) is x + y*128 so (4 + 128 = 132) */
 
 infinite_loop:
 	br infinite_loop
 
 erase_screen:
-	movia r16, MAX_X
-	movia r17, MAX_Y
+	movi r16, MAX_X
+	movi r17, MAX_Y 
 	mov r18, r0
 	movui r10, COL_BLACK
 	erase_screen_while_y:
@@ -65,9 +70,13 @@ draw_rect:
 	ldw r17, 4(sp)
 	mov r18, r0
 	ldw r10, 8(sp)
+	slli r4, r4, 1
+	slli r5, r5, 10
 	
 	draw_rect_while_y:
 		movia r8, ADDR_VGA
+		add r8, r8, r4
+		add r8, r8, r5
 		mov r19, r0
 		slli r20, r18, 10
 		add r8, r8, r20
